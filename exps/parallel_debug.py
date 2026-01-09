83163c56760b5baf346893939da9acb19a1745d3
jackopenn <jackcpennington@gmail.com>
2026-01-09 12:51:29 +0000

    simple muon

--- exps/parallel_debug.py
+++ exps/parallel_debug.py
@@ -5,34 +5,11 @@ from jax import numpy as jnp
 import optax
 from sws import Config
 
-# def warmup_linear_decay_schedule(
-#     init_value,
-#     peak_value,
-#     end_value,
-#     warmup_steps,
-#     decay_steps,
-#     max_steps
-# ):
-#     def schedule(step):
-#         warmup_pct = step / jnp.maximum(warmup_steps, 1)
-#         warmup_value = init_value + (peak_value - init_value) * warmup_pct
-        
-#         decay_start = max_steps - decay_steps
-#         decay_pct = (step - decay_start) / jnp.maximum(decay_steps, 1)
-#         decay_value = peak_value + (end_value - peak_value) * decay_pct
-        
-#         return jnp.where(
-#             step < warmup_steps,
-#             warmup_value,
-#             jnp.where(step < decay_start, peak_value, decay_value)
-#         )
-#     return schedule
-
 
 def get_config():
     cfg = Config()
     cfg.seed = 42
-    cfg.exp_name = "nanochat100-base"
+    cfg.exp_name = "nanochat100-debug"
 
     cfg.model.vocab_size = 50304
     cfg.model.num_layers = 1
@@ -68,51 +45,30 @@ def get_config():
     cfg.data.max_length = lambda: cfg.model.max_seq_len
 
     # target batch size of 524288 tokens (2048 seq_len)
-    cfg.data.batch_size = 32
-    cfg.optim.accum_steps = 8
+    cfg.data.batch_size = 4
+    cfg.optim.accum_steps = 1
 
     max_steps = 25_000
     cfg.max_steps = max_steps
     cfg.generate_every = 500
     cfg.eval_every = -1
     cfg.checkpoint_every = 5000
-    cfg.checkpoint_dir = "gs://trm-jax-123/jaximus/checkpoints/nanochat100-base"
+    cfg.checkpoint_dir = "checkpoints"
     cfg.xpu = "v4"
-    cfg.wandb = True
+    cfg.wandb = False
 
-    cfg.parallel.strategy = "fsdp"
-    cfg.parallel.data = 16
+    cfg.parallel.strategy = "dp"
+    cfg.parallel.data = 1
 
 
     # ---------- optimizer config ----------
-    # learning_rate_schedule = partial(warmup_linear_decay_schedule,
-    #     init_value=0.0,
-    #     end_value=0.0,
-    #     warmup_steps= 0.0 * max_steps,
-    #     decay_steps= 0.2 * max_steps,
-    #     max_steps=max_steps,
-    # )
-    # adamw_params = dict(weight_decay=0.0, eps=1e-10, b1=0.8, b2=0.95)
-    # cfg.optim.tx = lambda: optax.chain(
-    #     optax.clip_by_global_norm(1.0),
-    #     optax.partition(
-    #         {
-    #             "token_embedding": optax.adamw(
-    #                 learning_rate=learning_rate_schedule(peak_value=0.2 * ((cfg.model.hidden_dim / 768) ** -0.5)),
-    #                 **adamw_params,
-    #             ),
-    #             "lm_head": optax.adamw(
-    #                 learning_rate=learning_rate_schedule(peak_value=0.004 * ((cfg.model.hidden_dim / 768) ** -0.5)),
-    #                 **adamw_params,
-    #             ),
-    #             "other": optax.contrib.muon(
-    #                 learning_rate=learning_rate_schedule(peak_value=0.02),
-    #                 nesterov=True,
-    #                 beta=0.95,
-    #             ),
-    #         },
-    #         lambda state: jax.tree.map_with_path(lambda path, _: path[0].key if path[0].key in ("token_embedding", "lm_head") else "other", state)
-    #     )
-    # )
+    cfg.optim.warmup_pct = 0.0
+    cfg.optim.decay_pct = 0.4
+    cfg.optim.te_peak_value = lambda: 0.3 * ((cfg.model.hidden_dim / 768) ** -0.5)
+    cfg.optim.lm_head_peak_value = lambda: 0.004 * ((cfg.model.hidden_dim / 768) ** -0.5)
+    cfg.optim.other_peak_value = 0.02
+    cfg.optim.muon_momentum_start = 0.85
+    cfg.optim.muon_momentum_end = 0.95
+    cfg.optim.muon_momentum_warmup_steps = 300
 
     return cfg
