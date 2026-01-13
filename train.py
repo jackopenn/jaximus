a47d3c29f3b07cf4e91151f211e87edc411c0835
jackopenn <jackcpennington@gmail.com>
2026-01-13 17:20:52 +0000

    functional refactor

--- train.py
+++ train.py
@@ -82,15 +82,7 @@ def make_train_step(optimizer, model_config, model_weights, opt_weights):
     return train_step, input_sharding
 
 def train(cfg):
-    print("starting train")
-    print(f"{jax.process_index()=}")
-    print(f"{jax.process_count()=}")
-    print(f"{jax.local_device_count()=}")
-    print(f"{jax.devices()=}")
-    print(f"{jax.local_devices()=}")
-    print(f"{jax.devices('gpu')=}")
-    print(f"{jax.local_devices('gpu')=}")
-    exit()
+    print(cfg.to_dict())
 
     # init mesh
     mesh = jax.make_mesh((cfg.parallel.data, ), ("data", ), (AxisType.Explicit,))
